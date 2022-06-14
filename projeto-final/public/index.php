ea86fd7c5abe24c799eb736a07c0c9f6d9766962
arthur-fellipe <arthurfellipe@gmail.com>
2022-06-13 23:23:49 -0300

    Inclui atividades da aula09

--- projeto-final/public/index.php
+++ projeto-final/public/index.php
@@ -2,37 +2,54 @@
 
 include '../vendor/autoload.php';
 
-use App\Controller\IndexController;
-use App\Controller\ProductController;
-use App\Controller\CategoryController;
-use App\Controller\ErrorController;
+$database = 'db_store';
+$username = 'root';
+$password = '120396';
 
-$url = explode('?', $_SERVER['REQUEST_URI'])[0];
+$connection = new PDO('mysql:host=localhost;dbname='.$database, $username, $password);
 
-function createRoute(string $controllerName, string $methodName) {
-    return [
-        'controller' => $controllerName,
-        'method' => $methodName,
-    ];
-}
+$query = 'SELECT * FROM tb_category;';
 
-$routes = [
-    '/' => createRoute(IndexController::class, 'indexAction'),
-    '/login' => createRoute(IndexController::class, 'loginAction'),
-    '/product' => createRoute(ProductController::class, 'listAction'),
-    '/product/new' => createRoute(ProductController::class, 'addAction'),
-    '/product/edit' => createRoute(ProductController::class, 'editAction'),
-    '/category' => createRoute(CategoryController::class, 'listAction'),
-    '/category/new' => createRoute(CategoryController::class, 'addAction'),
-    '/category/edit' => createRoute(CategoryController::class, 'editAction'),
-];
+$preparacao = $connection->prepare($query);
+$preparacao->execute();
 
-if (false === isset($routes[$url])) {
-    (new ErrorController())->notFoundAction();
-    exit;
-}
+var_dump($preparacao->fetch());
 
-$controllerName = $routes[$url]['controller'];
-$methodName = $routes[$url]['method'];
+// while ($registro = $preparacao->fetch()) {
+//     var_dump($registro);
+// }
 
-(new $controllerName())->$methodName();
+// use App\Controller\IndexController;
+// use App\Controller\ProductController;
+// use App\Controller\CategoryController;
+// use App\Controller\ErrorController;
+
+// $url = explode('?', $_SERVER['REQUEST_URI'])[0];
+
+// function createRoute(string $controllerName, string $methodName) {
+//     return [
+//         'controller' => $controllerName,
+//         'method' => $methodName,
+//     ];
+// }
+
+// $routes = [
+//     '/' => createRoute(IndexController::class, 'indexAction'),
+//     '/login' => createRoute(IndexController::class, 'loginAction'),
+//     '/product' => createRoute(ProductController::class, 'listAction'),
+//     '/product/new' => createRoute(ProductController::class, 'addAction'),
+//     '/product/edit' => createRoute(ProductController::class, 'editAction'),
+//     '/category' => createRoute(CategoryController::class, 'listAction'),
+//     '/category/new' => createRoute(CategoryController::class, 'addAction'),
+//     '/category/edit' => createRoute(CategoryController::class, 'editAction'),
+// ];
+
+// if (false === isset($routes[$url])) {
+//     (new ErrorController())->notFoundAction();
+//     exit;
+// }
+
+// $controllerName = $routes[$url]['controller'];
+// $methodName = $routes[$url]['method'];
+
+// (new $controllerName())->$methodName();
